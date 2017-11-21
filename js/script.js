be1ce3dbcacf979492ac6039967f9021ef16aad7
ACE <acetip@aces-mbp.lab.lagcc>
2017-11-20 20:16:10 -0500

    finished foundation of fite night

--- js/script.js
+++ js/script.js
@@ -1,21 +1,52 @@
 class Fighter {
-  constructor(name, slogan, superPowers, armorModifier){
+  constructor(name, slogan, armorModifier, superPower1, superPower2, superPower3){
     this.name = name;
     this.slogan = slogan;
-    this.superPowers = [ ];
-    this.life = 100;
-    this.armor = 100;
+    this.superPowers = [superPower1, superPower2, superPower3];
+    this.baseLife = 50;
+    this.armor = 10;
+    this.armorModifier = armorModifier;
+    this.life = this.baseLife + this.armor;
   }
 
   getWeapon() {
-   var weaponNumber = Math.floor(Math.random() * (Math.floor(4)-Math.ceil(1)) + Math.ceil)
+   var weaponNumber = Math.floor(Math.random() * (Math.floor(4)-Math.ceil(1)) + Math.ceil(1));
 
    if (weaponNumber === 1) {
     this.weapon = 'mop';
+    this.weaponPower = 10;
   } else if (weaponNumber === 2) {
     this.weapon = 'chair';
+    this.weaponPower = 20;
    } else {
     this.weapon = 'ladder';
+    this.weaponPower = 30;
    }
   }
+
+}
+
+var kurt = new Fighter('Kurt', 'hello');
+var dawn = new Fighter('Dawn', 'goodbye');
+
+function battle(fighter1, fighter2) {
+  console.log(fighter1.slogan);
+  console.log(fighter2.slogan);
+  fighter1.getWeapon();
+  fighter2.getWeapon();
+  while ((fighter1.life > 0) && (fighter2.life > 0)) {
+    fighter1.life -= fighter2.weaponPower;
+    fighter2.life -= fighter1.weaponPower;
+  }
+
+  if ((fighter1.life <= 0) && (fighter2.life > 0)) {
+    console.log(fighter2.name + " wins!!!");
+  } else if ((fighter2.life <= 0) && (fighter1.life > 0)) {
+    console.log(fighter1.name + " wins!!!");
+  } else {
+    console.log("Both fighters have passed out!!! It's a draw!!!");
+  }
+
+  fighter1.life = 50 + fighter1.armor;
+  fighter2.life = 50 + fighter2.armor;
 }
